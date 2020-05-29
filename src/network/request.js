cc8a6ea070376aa4bbd603b7034a1004b42619d9
wushilaowang@163.com <wushilaowang@163.com>
2020-05-29 18:50:41 +0800

    5-29

--- src/network/request.js
+++ src/network/request.js
@@ -1,7 +1,6 @@
 import axios from 'axios'
 
 export default  function request(options) {
-    console.log(process.env.VUE_APP_BASEURL)
     const instance = axios.create({
         baseURL: process.env.VUE_APP_BASEURL,
         timeout: 20000
--- src/views/Word.vue
+++ src/views/Word.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div v-if="wordList.length">
         <div>{{wordList[currentIndex].word}}</div>
         <div>{{wordList[currentIndex].paraphrase}}</div>
         <label>循环次数</label><input v-model="loopCount" />
@@ -18,57 +18,90 @@ export default {
             currentIndex: 0,//当前序号
             currentPage: 1,//当前单词页数
             loopCount: 1,//循环次数
-            wordVoices: []
+            wordVoices: [],
+            isPlay: false,//是否播放
+            pageLoad: false,
         }
     },
     created() {
-        let param = {
-            url: '/getWord',
-            param: {
-                page: 1,
-                pageSize: 10
-            }
-        }
-        word(param).then(res => {
-            this.wordList = res.data
-        })
+        this.loadWord();
+        this.pageLoad = true
     },
     methods: {
+        //加载单词
+        loadWord() {
+            let that = this;
+            let param = {
+                url: '/getWord',
+                param: {
+                    page: this.currentPage,
+                    pageSize: 2
+                }
+            }
+            word(param).then(res => {
+                res.data.map(item => {
+                    that.wordList.push(item)
+                })
+            })
+        },
         handlePlay() {
             const that = this
+            console.log(this.wordList)
             //获取元素
             let audio = document.querySelector('#wordAudio');
-            //循环次数 audio src数组
-            for(let i = 0; i < this.loopCount; i++){
-                this.wordVoices.push("http://dict.youdao.com/dictvoice?audio="+this.wordList[this.currentIndex].word+"&type=1")
-                this.wordVoices.push("https://fanyi.baidu.com/gettts?lan=zh&text="+this.wordList[this.currentIndex].paraphrase+"&spd=5&source=web")
-            }
+            that.loadVoicSrc()
             console.log(this.wordVoices)
             //首个src
             let src = this.wordVoices.shift();
             audio.src = src;
+            audio.play();
             console.log(this.wordVoices)
             //播放完触发
             audio.addEventListener('ended', function() {
-                console.log(that.wordVoices.length)
-                if(that.wordVoices.length > 0) {
+                console.log(that.wordList.length)
+                if(that.wordVoices.length > 0) {//播放列表中还有
                     audio.src = that.wordVoices.shift()
                     audio.play()
                     console.log(audio.src)
                 }else {
-                    if(that.wordList.length > that.currentIndex) {
-                        that.currentIndex = ++that.currentIndex;
-                        that.handlePlay() 
+                    console.log(that.wordList.length > that.currentIndex + 1)
+                    if(that.wordList.length > that.currentIndex + 1) {//加载单词的数量 > 当前单词序号
+                        that.currentIndex = that.currentIndex + 1;
+                        that.loadVoicSrc();
+                        audio.src = that.wordVoices.shift();
+                        audio.play();
+                    }else {//再次加载单词
+                        console.log(that.wordVoices)
+                        that.currentPage = that.currentPage + 1;
+                        that.loadWord();
+                        console.log(that.wordList)
+                        that.currentIndex = that.currentIndex + 1;
+                        that.loadVoicSrc();
+                        audio.src = that.wordVoices.shift();
+                        audio.play();
                     }
                 }
             });
-            
-            audio.play();
         },
+        //加载src
+        loadVoicSrc() {
+            let arr = this.wordList
+            JSON.parse(JSON.stringify(arr))
+            console.log(this.currentIndex)
+            console.log(arr)
+            //循环次数 audio src数组
+            for(let i = 0; i < this.loopCount; i++){
+                this.wordVoices.push("http://dict.youdao.com/dictvoice?audio="+this.wordList[this.currentIndex].word+"&type=1")
+                // this.wordVoices.push("https://fanyi.baidu.com/gettts?lan=zh&text="+this.wordList[this.currentIndex].paraphrase+"&spd=5&source=web")
+            }
+        }
     }
 }
 </script>
 
 <style scoped>
 
+[v-cloak] {
+    display: none;
+}
 </style>
